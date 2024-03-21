d0921d18532641e6c96eba7cd8cfe0d25b34f6e4
Mayhul-Jindal <mayhuljindal@gmail.com>
2024-03-21 16:18:18 +0530

    fixing conflicts

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 
 	"github.com/tanx-libs/tanx-connector-go/client"
@@ -21,11 +22,11 @@ func main() {
 
 	ethAddr := "0xf318C11ff6E60115FB3e107bEa2637c060BEbc8C"
 	ethPrivateKey := "ba169c79340371a9aa4fd516462f939242f92b522081d945c001b0fb3dc3a66f"
-	// starkPublicKey := "0x64211ed550cb37140ef2268cf7b2625aef725d33618c9651765e16318101c17"
+	starkPublicKey := "0x64211ed550cb37140ef2268cf7b2625aef725d33618c9651765e16318101c17"
 	// starkPrivateKey := "0x7302fa58776da9f8fcf3631f4cb495a4dd0cdfab785e8b72a8a637d4bb14784"
 
 	// rpcURL := "https://sepolia.infura.io/v3/bc9fafffa1f447bab403ee4a8b5090f4"
-	_, _, err = c.Login(context.TODO(), ethAddr, ethPrivateKey)
+	_, err = c.Login(context.TODO(), ethAddr, ethPrivateKey)
 	if err != nil {
 		panic(err)
 	}
